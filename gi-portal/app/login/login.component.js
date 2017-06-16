2323e325999b3edbf967b38c0057d407988ee945
nikhil <nikhil@knoldus.com>
2017-06-16 15:27:00 +0530

    Added user login ui #27

---
+++ gi-portal/app/login/login.component.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+require('rxjs/add/observable/throw');
+var LoginComponent = (function () {
+    function LoginComponent() {
+    }
+    LoginComponent.prototype.ngOnInit = function () {
+        console.log("login rendered");
+        // $('[data-toggle="checkbox"]').each(function () {
+        //     if($(this).data('toggle') == 'switch') return;
+        //
+        //     var $checkbox = $(this);
+        //     $checkbox.checkbox();
+        // });
+        //initDemo();
+    };
+    LoginComponent = __decorate([
+        core_1.Component({
+            selector: 'login-cmp',
+            moduleId: module.id,
+            styleUrls: ['./login.component.style.css'],
+            templateUrl: 'login.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], LoginComponent);
+    return LoginComponent;
+}());
+exports.LoginComponent = LoginComponent;
+//# sourceMappingURL=login.component.js.map
