0b030633b087e94d6a849a37acc47a4e222e4978
Rishi Khandelwal <rishi@knoldus.com>
2017-06-19 10:28:54 +0530

    Removed main method from object and extended with App

--- gi-user/src/main/scala/com/knoldus/user/UserBoot.scala
+++ gi-user/src/main/scala/com/knoldus/user/UserBoot.scala
@@ -6,12 +6,12 @@ import akka.stream.ActorMaterializer
 import com.knoldus.user.Constants.Port
 import com.knoldus.user.api.UserApi
 
-object UserBoot extends UserApi {
+object UserBoot extends App {
 
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("api-actor-system")
-    implicit val materializer = ActorMaterializer()
-    Http().bindAndHandle(addUser, "localhost", Port)
-  }
+  val userApi = new UserApi
+
+  implicit val system = ActorSystem("api-actor-system")
+  implicit val materializer = ActorMaterializer()
+  Http().bindAndHandle(userApi.addUser, "localhost", Port)
 
 }
