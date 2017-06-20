8cae02b5db9160a17b50bdcb353260700531ef8d
anurag <anurag@knoldus.com>
2017-06-20 13:21:21 +0530

    Update code for various retieve option

--- gi-persistence/src/main/scala/com/knoldus/persistence/UserComponent.scala
+++ gi-persistence/src/main/scala/com/knoldus/persistence/UserComponent.scala
@@ -1,16 +1,13 @@
 package com.knoldus.persistence
 
-import java.sql.Timestamp
-
 import com.knoldus.persistence.mappings.UserMapping
 import com.knoldus.utils.models.User
-
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
+import scala.concurrent.Future
 
 trait UserComponent extends UserMapping with PostgresDbComponent {
 
   import driver.api._
+
   /**
     * This method is used for insert user into database
     *
@@ -18,9 +15,7 @@ trait UserComponent extends UserMapping with PostgresDbComponent {
     * @return
     */
   def insert(user: User): Future[Int] = {
-    val res = db.run(userInfo += user)
-     Await.result(res, 2 seconds)
-    res
+    db.run(userInfo += user)
   }
 
   /**
@@ -31,7 +26,7 @@ trait UserComponent extends UserMapping with PostgresDbComponent {
     * @return Option[User]
     */
   def getUserViaEmailAndPassword(email: String, password: String): Future[Option[User]] = {
-    db.run(userInfo.filter(u => u.email === email && u.password === password).result.headOption)
+    db.run(userInfo.filter(user => user.email === email && user.password === password).result.headOption)
   }
 
   /**
@@ -41,7 +36,7 @@ trait UserComponent extends UserMapping with PostgresDbComponent {
     * @return Option[User]
     */
   def getUserViaUserId(userId: String): Future[Option[User]] = {
-    db.run(userInfo.filter(u => u.id === userId).result.headOption)
+    db.run(userInfo.filter(user => user.id === userId).result.headOption)
   }
 
   /**
@@ -51,13 +46,6 @@ trait UserComponent extends UserMapping with PostgresDbComponent {
     * @return Option[User]
     */
   def getUserViaAccessToken(accessToken: String): Future[Option[User]] = {
-    db.run(userInfo.filter(u => u.accessToken === accessToken).result.headOption)
+    db.run(userInfo.filter(user => user.accessToken === accessToken).result.headOption)
   }
-
-  val time = new Timestamp(System.currentTimeMillis())
-  val user: User = User("12", "jjjjjjjjj", "1024", "anurag", "anurag@knoldus.com", "anurag", "admin", time, time)
-  insert(user)
-  /*getUserViaAccessToken("jjjjjjjjj").map(s => println("!!!!!!!!!!!!!!!!!!!! a t " + s.get.email))
-  getUserViaEmailAndPassword("anurag@knoldus.com","anurag").map(s => println("!!!!!!!!!!!!!!!!!!!! E P " + s.get.name))
-  getUserViaUserId("12").map(s => println("!!!!!!!!!!!!!!!!!!!! User ID " + s.get.name))*/
 }
