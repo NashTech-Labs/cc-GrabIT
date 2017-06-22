24d7943c3aff8cf6efc824b7c6b7dc786dcfd42a
Jyotsna <jyotsna@knoldus.com>
2017-06-22 13:03:18 +0530

    1. Intergrated circe library for handling json 2. Removed usage of spray json 3. Updated test cases

--- gi-user/src/main/scala/com/knoldus/user/api/UserApi.scala
+++ gi-user/src/main/scala/com/knoldus/user/api/UserApi.scala
@@ -4,12 +4,17 @@ import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.google.inject.Inject
-import com.knoldus.user.JsonHelper._
+import com.knoldus.user.helper.UserApiHepler
 import com.knoldus.user.model.{SignInRequest, UserRegisterRequest}
 import com.knoldus.user.service.UserService
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+
 import scala.util.{Failure, Success}
 
-class UserApi @Inject()(userService: UserService ) {
+
+class UserApi @Inject()(userService: UserService ) extends UserApiHepler {
 
   /**
     * Creates http route for add user
@@ -17,11 +22,12 @@ class UserApi @Inject()(userService: UserService ) {
     */
   def addUser: Route =
   path("user" / "add") {
-    (post & entity(as[UserRegisterRequest])) { userRegisterRequest =>
+    (post & entity(as[String])) { data =>
       parameters("accessToken") { accessToken =>
-        onComplete(userService.addUser(userRegisterRequest)) {
-          case Success(res) => complete(HttpResponse(StatusCodes.OK, entity = s"User has been Successfully Added"))
-          case Failure(ex) => complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Internal Server Error ${ex.getMessage}"))
+        val decodedUserRequest = decode[UserRegisterRequest](data)
+        decodedUserRequest match {
+          case Right(userRegisterRequest) => handleAddUser(userRegisterRequest, userService.addUser)
+          case Left(ex) => complete(HttpResponse(StatusCodes.BadRequest, entity = s"Body params are missing or incorrect: ${ex.getMessage}"))
         }
       }
     }
@@ -33,11 +39,12 @@ class UserApi @Inject()(userService: UserService ) {
     */
   def signIn: Route =
   path("signin") {
-    (post & entity(as[SignInRequest])) { signInRequest =>
-      onComplete(userService.signIn(signInRequest)) {
-        case Success(Some(user)) => complete(user)
-        case Success(None) => complete(HttpResponse(StatusCodes.BadRequest, entity = "Invalid credentials"))
-        case Failure(ex) => complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Internal Server Error ${ex.getMessage}"))
+    (post & entity(as[String])) { data =>
+      val decodedSignInRequest = decode[SignInRequest](data)
+
+      decodedSignInRequest match {
+        case Right(signInRequest) => handleSignIn(signInRequest, userService.signIn)
+        case Left(ex) => complete(HttpResponse(StatusCodes.BadRequest, entity = s"${ex.getMessage}"))
       }
     }
   }
@@ -48,9 +55,9 @@ class UserApi @Inject()(userService: UserService ) {
     */
   def getAllUsers: Route =
   path("user" / "get" / "all") {
-    (get) {
+    get {
       onComplete(userService.getAllUsers) {
-        case Success(users) => complete(users)
+        case Success(users) => complete(HttpResponse(StatusCodes.OK, entity = users.asJson.toString))
         case Failure(ex) => complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Internal Server Error ${ex.getMessage}"))
       }
     }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+addSbtPlugin("com.codacy" % "sbt-codacy-coverage" % "1.3.8")
