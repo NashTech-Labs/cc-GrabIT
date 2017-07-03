f2d472d9e9d009d22fd41c5ba6d67ba7b8a8ac7c
Jyotsna <jyotsna@knoldus.com>
2017-07-03 11:31:50 +0530

    Updated user and asset list when new data added #88

--- gi-portal/app/manage/users.component.js
+++ gi-portal/app/manage/users.component.js
@@ -28,6 +28,9 @@ var UserComponent = (function () {
         ];
     }
     UserComponent.prototype.ngOnInit = function () {
+        this.updateUserList();
+    };
+    UserComponent.prototype.updateUserList = function () {
         var _this = this;
         // Getting the list of users when users view appear
         this.usersService.getUserList().subscribe(function (data) {
@@ -46,6 +49,7 @@ var UserComponent = (function () {
             _this.returnedUseraddResponse = data;
             _this.formValues = value;
             swal('Good job!', 'New user has been added!', 'success');
+            _this.updateUserList();
             jQuery(".modal-body input").val("");
             jQuery('#newUserModal').modal('hide');
         }, function (error) {
